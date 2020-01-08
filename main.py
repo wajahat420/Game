a34225a8e4344cdd05288d2ae918dd3fe9695fb6
wajahat420 <shayanmustafa11@gmail.com>
2020-01-08 22:24:45 +0500

    ball moving correct and clashing right

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ speed = [0.3, 0]
 black = 0, 0, 0
 white = 255, 255, 255
 brown = 139,69,19
-
+blue = 0,0,255
 screen = pygame.display.set_mode(size)
 
 # ball = pygame.image.load("ball.png")
@@ -23,18 +23,27 @@ h2 = 400
 h3 = 200
 
 ball_height = 300
+ball_x = 70
 rect_speed = 0.2
 iterate_one_time = 0
 up = False
 counter = 0
+radius = 18
+gap = 120
 
 
-def crash():
+def check(x,y):
+	font = pygame.font.SysFont("comicsansms", 40)
+	text = font.render(".", True, blue)
+	screen.blit(text,(x , y))
+	
+def crash(arg):
+	print("arg",arg)
 	font = pygame.font.SysFont("comicsansms", 80)
 	text = font.render("YOU CRASHED", True, (255, 255, 255))
-	gameDisplay.blit(text,(100,150))
-	pygame.display.update()
+
 	time.sleep(2)
+	screen.blit(text,(-1000,150))
 
 while 1:
 	for event in pygame.event.get():
@@ -42,30 +51,45 @@ while 1:
 
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_UP:
-				ball_height -= 40
-				print("up",up)
+				counter = 0
 				up = True
-		# if event.type == pygame.KEYUP:
-		# 	if event.key == pygame.K_UP:
-		# 		ball_height += 1
-		# if event.type == pygame.KEYUP:
-		# 	if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-		# 		x_change = 0
-	# print("up",up)
+	
 	if not up:
 		# print("if")
-		ball_height += 0.1
-	else:
+		ball_height += 0.38
 
+	else:
+		# print("else")
 		counter += 1
-		if counter > 5000:
+		ball_height -= 0.46
+		if counter > 120 :
+			counter = 0
 			up = False
 
 	x1 -= rect_speed
 	x2 -= rect_speed
 	x3 -= rect_speed
 
+	y1 = h1 - 45 
+	y2 = h2 - 45 
+	y3 = h3 - 45 
+	
+	ball_right = ball_x + radius - 15 
+	ball_left = ball_x - radius 
+	ball_top = ball_height - 40 - radius 
+	ball_bottom = ball_height -radius -10
 
+
+	if (ball_right > x1 and ball_left < x1) and  (ball_top < y1 or  ball_bottom > y1 + gap):
+		time.sleep(0.5)
+
+	elif (ball_right > x2 and ball_left < x2) and  (ball_top < y2 or  ball_bottom > y2 + gap):
+			time.sleep(0.5)
+
+	elif (ball_right > x3 and ball_left < x3) and  (ball_top < y3 or  ball_bottom > y3 + gap):
+		time.sleep(0.5)
+
+		
 	if x1 < 0:
 		h1 = random.randint(200, 400)
 		x1 = width
@@ -78,22 +102,21 @@ while 1:
 
 
 	
-	
 	screen.fill(white)
-	# screen.blit(ball, ballrect)
 
 	pygame.draw.rect(screen,black ,[x1,0,27,h1])
-	pygame.draw.rect(screen,black ,[x1, h1 + 80 , 27,500])
+	pygame.draw.rect(screen,black ,[x1, h1 + gap , 27,500])
 	pygame.draw.rect(screen,black ,[x2,0,27,h2])
-	pygame.draw.rect(screen,black ,[x2,h2 + 80,27,500])
+	pygame.draw.rect(screen,black ,[x2,h2 + gap,27,500])
 	pygame.draw.rect(screen,black ,[x3,0,27,h3])
-	pygame.draw.rect(screen,black ,[x3,h3 + 80, 27,500])
+	pygame.draw.rect(screen,black ,[x3,h3 + gap, 27,500])
 	
-	pygame.draw.circle(screen, brown , (70,int(ball_height)), 18)
+	pygame.draw.circle(screen, brown , (ball_x,int(ball_height)), radius)
+	check(x2,y2)
 
 
 	pygame.display.flip()
-	# if iterate_one_time == 0:
-	# 	time.sleep(0.8)
+	if iterate_one_time == 0:
+		time.sleep(0.8)
 	iterate_one_time = 1
 	# time.sleep(0.2)
